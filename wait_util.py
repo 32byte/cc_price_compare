566494714221a0d3216b9afbe757641b163cff7f
byteco <xlebedenko@gmail.com>
2020-08-04 15:59:19 +0200

    Added wait util to display text while waiting

---
+++ wait_util.py
@@ -0,0 +1,15 @@
+from threading import _start_new_thread
+import time
+
+def _wait_for(bool, text, blinc, end, delay):
+    print(text)
+    i = 0
+    while bool[0] > 0:
+        print('\033[1A\033[{}C{}'.format(len(text), blinc[i % len(blinc)]))
+        i += 1
+        bool[0] -= 1
+        time.sleep(delay)
+    print('\033[1A\033[{}C{}'.format(len(text), end))
+
+def wait_for(bool, text, blinc=['-','/','|','\\'], end=' done!', delay=.7):
+    _start_new_thread(_wait_for, (bool, text, ['-','/','|','\\'], ' done!', delay, ))
