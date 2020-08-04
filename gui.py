da10c1826246a3bb10eef051da69750d86446679
xWingZz <lukas.fritzl@hotmail.com>
2020-08-04 09:35:53 +0200

    Added App class in gui.py

--- gui.py
+++ gui.py
@@ -1,12 +1,9 @@
-from tkinter import *
+import tkinter as tk
 
-master = Tk()
+class App(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
 
-title = Label(master, text="Compare Game prices").grid(row=0)
+        self.x = self.y = 0
 
-l1 = Label(master, text="Search for Game:", justify=LEFT).grid(row=1)
-e1 = Entry(master, )
-
-e1.grid(row=1, column=1)
-
-master.mainloop()
+        
