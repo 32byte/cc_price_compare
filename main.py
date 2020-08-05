dd0d827efb7afec365435d970a9e3bb555684677
byteco <xlebedenko@gmail.com>
2020-08-05 14:18:42 +0200

    Added argparse to cli

--- main.py
+++ main.py
@@ -7,29 +7,35 @@ import time
 
 def get_prices(query, dlc=False, max=3):
     out = []
+    error = [False]
     try:
+        error[0] = False
         waiting = [True]
-        
-        wait_util.wait_for(waiting, 'Connecting to instant-gaming ')
+        wait_util.wait_for(waiting, error, 'Connecting to instant-gaming ')
         out += instant_gaming.get_prices(query, dlc, max)
         waiting[0] = False
         time.sleep(.8)
     except Exception:
-        pass
+        error[0] = True
+        time.sleep(.8)
     try:
+        error[0] = False
         waiting = [True]
-        wait_util.wait_for(waiting, 'Connecting to steam ')
+        wait_util.wait_for(waiting, error, 'Connecting to steam ')
         out += steam.get_prices(query, dlc, max)
         waiting[0] = False
         time.sleep(.8)
     except Exception:
-        pass
+        error[0] = True
+        time.sleep(.8)
     try:
+        error[0] = False
         waiting = [True]
-        wait_util.wait_for(waiting, 'Connecting to g2a ')
+        wait_util.wait_for(waiting, error, 'Connecting to g2a ')
         out += g2a.get_prices(query, dlc, max)
         waiting[0] = False
         time.sleep(.8)
     except Exception:
-        pass
+        error[0] = True
+        time.sleep(.8)
     return out
--- wait_util.py
+++ wait_util.py
@@ -1,14 +1,17 @@
 from threading import _start_new_thread
 import time
 
-def _wait_for(bool, text, blinc, end, delay):
+def _wait_for(bool, err, text, blinc, end, errtxt, delay):
     print(text)
     i = 0
-    while bool[0] > 0:
+    while bool[0] and not err[0]:
         print('\033[1A\033[{}C{}'.format(len(text), blinc[i % len(blinc)]))
         i += 1
         time.sleep(delay)
-    print('\033[1A\033[{}C{}'.format(len(text), end))
+    if err[0]:
+        print('\033[1A\033[{}C{}'.format(len(text), '\033[91m' + errtxt + '\033[0m'))
+    else:
+        print('\033[1A\033[{}C{}'.format(len(text), '\033[92m' + end + '\033[0m'))
 
-def wait_for(bool, text, blinc=['-','/','|','\\'], end=' done!', delay=.7):
-    _start_new_thread(_wait_for, (bool, text, ['-','/','|','\\'], ' done!', delay, ))
+def wait_for(bool, err, text, blinc=['-','/','|','\\'], end='done!', errtxt='error!', delay=.7):
+    _start_new_thread(_wait_for, (bool, err, text, blinc, end, errtxt, delay, ))
